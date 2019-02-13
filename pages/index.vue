d937d8e503668b82ef36d5a614b00c77fbec04a8
majun36 <majun1@jd.com>
2019-02-13 15:05:07 +0800

    支持 scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,28 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        mt-app
-      </h1>
-      <h2 class="subtitle">
-        My doozie Nuxt.js project
-      </h2>
+      <logo/>
+      <h1 class="title">mt-app</h1>
+      <h2 class="subtitle">My doozie Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -45,10 +33,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
-  font-weight: 300;
+  font-weight: 600;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
