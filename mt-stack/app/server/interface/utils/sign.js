f55ec2ee6d7e86e5e47432c96a71e32c75bb4179
majun36 <majun1@jd.com>
2019-02-27 17:16:23 +0800

    add mt-stack

---
+++ mt-stack/app/server/interface/utils/sign.js
@@ -0,0 +1,2 @@
+const sign = '请填写自己的sign'
+export default sign
--- pages/about.vue
+++ pages/about.vue
@@ -17,7 +17,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" >
 .container {
   margin: 0 auto;
   min-height: 100vh;
