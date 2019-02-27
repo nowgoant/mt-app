cd092a114f6d817be0a2c1b7c39090a3324b12b7
majun36 <majun1@jd.com>
2019-02-27 16:44:09 +0800

    update

---
+++ pages/about.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">about page</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-weight: 600;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
