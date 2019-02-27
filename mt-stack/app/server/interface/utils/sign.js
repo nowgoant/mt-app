b16f0725d04ccf8fe7db1ad11d1d69eac46cbd79
majun36 <majun1@jd.com>
2019-02-27 17:22:26 +0800

    delete mt-stack

--- mt-stack/app/server/interface/utils/sign.js
+++
@@ -1,2 +0,0 @@
-const sign = '请填写自己的sign'
-export default sign
--- mt-stack/vue-learn/src/components/n.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-Vue.directive('n', {
-  bind: function(el, binding) {
-    el.textContent = Math.pow(binding.value, 2)
-  },
-  update: function(el, binding) {
-    el.textContent = Math.pow(binding.value, 2)
-  }
-})
